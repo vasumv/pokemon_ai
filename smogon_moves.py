21838123b85cff81d3a0d50949f8eeb70a0b0d35
vikramv <vasumvikram@gmail.com>
2014-12-01 22:40:39 -0800

    added more to move_list

--- smogon_moves.py
+++ smogon_moves.py
@@ -29,16 +29,29 @@ class SmogonMoves():
         if category == "Physical" or category == "Special":
             name = result[0]['name']
             power = result[0]['power']
+            description = result[0]['description']
+            priority = 0
+            if "Priority" in description:
+                priority_index = description.index("Priority")
+                priority = description[priority_index+9:][:2]
+                if "+" in priority:
+                    priority = priority[1:]
+                priority = int(priority)
+            accuracy = float(result[0]['accuracy']) / 100
             type = result[0]['type']['name']
             output = """"%s": DamagingMove("%s",
                           power=%d,
                           category="%s",
-                          type="%s"),\n""" % (
+                          priority=%d,
+                          type="%s",
+                          accuracy=%f),\n""" % (
                           name,
                           name,
                           power,
                           category,
-                          type
+                          priority,
+                          type,
+                          accuracy
                           )
             with open("data/moves.json", "a") as f:
                 f.write(output)
@@ -48,5 +61,7 @@ move = SmogonMoves()
 moves = move.get_all_moves()
 for poke_move in moves:
     print poke_move
+    if poke_move == "Pursuit":
+        continue
     info = move.get_move_info(poke_move)
     move.write_damage_move(poke_move, info)
