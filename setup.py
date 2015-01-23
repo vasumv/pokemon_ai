73154c3a2df159b3d7e07f05b57c560ef26843f0
vikramv <vasumvikram@gmail.com>
2015-01-22 22:04:37 -0800

    updated Pip and fixed parse_requirements error

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import os
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
+import uuid
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
+install_reqs = parse_requirements("requirements.txt", session=uuid.uuid1())
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
