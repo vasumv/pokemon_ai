d592d3b7596e7ce9fa796e122116de9eece491a9
vikramv <vasumvikram@gmail.com>
2015-01-26 19:38:44 -0800

    added log_scraper with bs4

--- log_scraper/log_scraper.py
+++ log_scraper/log_scraper.py
@@ -1,63 +1,41 @@
-import time
-import os
-from showdown_ai import Selenium
-from selenium.webdriver.common.keys import Keys
-selenium = Selenium(url='http://replay.pokemonshowdown.com/')
+from bs4 import BeautifulSoup
+from path import path
+import requests
 
-selenium.start_driver()
-user_input = raw_input("Enter username: ")
-username = selenium.driver.find_element_by_xpath("/html/body/div[2]/div/form/p/label/input")
-username.send_keys(user_input)
-username.send_keys(Keys.RETURN)
-while "Search results" not in selenium.driver.find_element_by_xpath("/html/body/div[2]/div/h1[2]").text:
-    print "checking for search thing"
-    time.sleep(1)
-for i in range(20):
-    link = selenium.driver.find_element_by_xpath("/html/body/div[2]/div/ul/li[%d]" % (i+4))
-    if "ou" not in link.get_attribute("href"):
-        continue
-    link.click()
-    while not selenium.check_exists_by_xpath("/html/body/div[3]/div/div/div[1]/div/button[2]"):
-        time.sleep(1)
-    play = selenium.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[1]/div/button[2]")
-    play.click()
-    while not selenium.check_exists_by_xpath("/html/body/div[3]/div/div/div[3]/button[3]"):
-        time.sleep(1)
-    #turn = selenium.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/button[4]")
-    #turn.click()
-    #try:
-        #turn.send_keys("100")
-    #except UnexpectedAlertPresentException:
-        #alert = selenium.driver.switch_to_alert()
-        #print "i got the alert"
-        #time.sleep(2)
-        #alert.send_keys('100')
-        #print "just sent 100"
-        #time.sleep(2)
-        #alert.send_keys(Keys.RETURN)
-        #print "just pressed enter"
-    time.sleep(2)
-    log = selenium.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]").text.encode('utf-8')
-    while "won the battle" not in log:
-        for i in range(5):
-            skip = selenium.driver.find_element_by_xpath('/html/body/div[3]/div/div/div[3]/button[3]')
-            skip.click()
-        log = selenium.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]").text.encode('utf-8')
-    time.sleep(3)
-    url = selenium.driver.current_url
-    url_list = url.split('-')
-    battle_id = 'ou-' + url_list[1]
-    if not os.path.exists("logs/%s" % (user_input)):
-        os.makedirs("logs/%s" % (user_input))
-    with open("logs/%s/%s.log" % (user_input, battle_id), "w") as f:
-        f.write(log)
-    selenium.driver.back()
-    time.sleep(2)
-#play = selenium.driver.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[1]/div/button[2]')
-#play.click()
-#for i in range(20):
-    #skip = selenium.driver.find_element_by_xpath('/html/body/div[2]/div/div[1]/div[3]/button[3]')
-    #skip.click()
-#time.sleep(5)
-#with open('log2.txt', 'w') as f:
-    #f.write(selenium.get_log().encode('utf-8'))
+def get_usernames():
+    with open("ladder_names.txt") as f:
+        usernames = f.read().split("\n")
+    return usernames
+
+def get_user_replays(username):
+    USERNAME_URL = "http://replay.pokemonshowdown.com/search/?output=html&user=%s" % username
+    html = requests.get(USERNAME_URL).text
+    soup = BeautifulSoup(html)
+    links = soup.find_all('a')
+    links = [link.get("href").encode("utf-8") for link in links if "ou" in link.get("href")]
+    return links
+
+def get_logs(link):
+    html = requests.get("http://replay.pokemonshowdown.com" + link).text
+    soup = BeautifulSoup(html)
+    script = soup.find_all('script', {'class': 'log'})[0]
+    log = script.text
+    return log.encode('utf-8')
+
+if __name__ == "__main__":
+    usernames = get_usernames()
+    links_list = []
+    for user in usernames:
+        print user
+        links = get_user_replays(user)
+        for link in links:
+            if link in links_list:
+                continue
+            directory = path("logs/%s" % user)
+            log = get_logs(link)
+            if not directory.exists():
+                directory.makedirs()
+            with open("logs/%s/%s.log" % (user, link[1:]), 'w') as f:
+                f.write(log)
+            links_list.append(link)
+
